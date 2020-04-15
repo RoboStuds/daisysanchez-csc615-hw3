b8a613e2e38787af0bfd7345f4b35bb54aaa288e
Daisy <Daisysanchez202012@gmail.com>
2020-04-14 22:22:51 -0700

    Code fixed

--- motor.c
+++ motor.c
@@ -1,72 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <softPwm.h>
+ 
+
 
 int main (void){
 
 wiringPiSetup();
 
-pinMode(7, OUTPUT);
-pinMode(11, OUTPUT);
-pinMode(12, OUTPUT);
-pinMode(13, OUTPUT);
-pinMode(15, OUTPUT);
-pinMode(16, OUTPUT);
-pinMode(18, OUTPUT);
+//Motor 1
+// 0 is to enable
+//13 and 15 to control
+pinMode(0, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+
+//Motor 2
+//6 is to enable
+//4 and 5 to control
+pinMode(6, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
 
 
 int var = 0;
   
   while(var < 2) {
 
-//Drive motor Clockwise
+//Motor 1 forward
+digitalWrite(0, HIGH);
+digitalWrite(2, HIGH);
+digitalWrite(3, LOW);
 
-//Motor A
-digitalWrite(12, HIGH);
-digitalWrite(11, LOW);
+//Motor 2 forward 
+digitalWrite(6, HIGH)
+digitalWrite(4, HIGH);
+digitalWrite(5, LOW);
 
-//Motor B
-digitalWrite(15, HIGH);
-digitalWrite(16, LOW);
-
-//Set the motor speed
-
-//Motor A
-digitalWrite(7, HIGH);
-
-//Motor B 
-digitalWrite(18, HIGH);
-
-//Disable
-digitalWrite(13, HIGH);
 delay(5000);
 
-//Drive  motor Counterclockwise
+//Motor 1 Backward 
+digitalWrite(0, HIGH);
+digitalWrite(2, LOW);
+digitalWrite(3, HIGH);
 
-//Motor A
-digitalWrite(12, LOW);
-digitalWrite(11, HIGH);
+//Motor 2 Backward
+digitalWrite(6, HIGH); 
+digitalWrite(4, LOW);
+digitalWrite(5, HIGH);
 
-//Motor B
-digitalWrite(15, LOW);
-digitalWrite(16, HIGH);
-
-//Set the motor speed
-digitalWrite(7, HIGH);
-digitalWrite(18, HIGH);
-
-//Diasble 
-digitalWrite(13, HIGH);
-delay(5000);
-
-//Reset Pins
-digitalWrite(12, LOW);
-digitalWrite(11, LOW);
-digitalWrite(7, LOW);
-digitalWrite(13, LOW);
-digitalWrite(15, LOW);
-digitalWrite(16, LOW);
-digitalWrite(18, LOW);
+var++;
 
     }
   return  0;
